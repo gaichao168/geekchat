8cf8a02c3694df8a3dc25f1b9e8a9fa541b96570
CodeClown <1432681790@qq.com>
2023-03-27 20:45:40 +0800

    添加谷歌统计

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,12 +1,13 @@
 {{--<!-- Google tag (gtag.js) -->--}}
-{{--<script async src="https://www.googletagmanager.com/gtag/js?id=G-MC8EE0SDSQ"></script>--}}
-{{--<script>--}}
-{{--    window.dataLayer = window.dataLayer || [];--}}
-{{--    function gtag(){dataLayer.push(arguments);}--}}
-{{--    gtag('js', new Date());--}}
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P3VDJ5H5DW"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-{{--    gtag('config', 'G-MC8EE0SDSQ');--}}
-{{--</script>--}}
+    gtag('config', 'G-P3VDJ5H5DW');
+</script>
 
 {{--百度统计--}}
 <script>
