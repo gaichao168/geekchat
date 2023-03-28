9dccb55d42af2cac2ba26b122059131c5d7f546b
CodeClown <1432681790@qq.com>
2023-03-28 22:44:08 +0800

    添加文章

--- resources/views/shares/show.blade.php
+++ resources/views/shares/show.blade.php
@@ -7,7 +7,14 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden min-h-screen shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h1 class="text-2xl font-bold pb-4 text-center">{{$share->title}}</h1>
-                    {!! $share->content !!}
+                    <div class="my-4">
+                        {!! $share->content !!}
+                    </div>
+
+                    <hr>
+                    <div class="mt-4 flex justify-end">
+                        发布时间：{{$share->created_at}}
+                    </div>
                 </div>
             </div>
         </div>
