fa888ceb5c1b78608a8366438505c70e24c3021e
DESKTOP-9IETHKG\Administrator <1432681790@qq.com>
2023-04-11 23:04:26 +0800

    调整公众号

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,9 +12,7 @@
 
     <!-- Scripts -->
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4193846562907716"
-            crossorigin="anonymous">
-
-    </script>
+            crossorigin="anonymous"></script>
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
