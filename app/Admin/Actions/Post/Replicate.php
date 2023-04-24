67861bd4cbb17637d6abec5db8cb0ee15234aab1
DESKTOP-9IETHKG\Administrator <1432681790@qq.com>
2023-04-25 00:30:10 +0800

    调整会员充值逻辑 调整up_vip_history表属性

--- app/Admin/Actions/Post/Replicate.php
+++ app/Admin/Actions/Post/Replicate.php
@@ -5,6 +5,7 @@ namespace App\Admin\Actions\Post;
 use App\Models\Admin\UpVipConfig;
 use App\Models\Admin\UpVipHistory;
 use App\Models\Admin\UserGptKeys;
+use Carbon\Carbon;
 use Encore\Admin\Actions\Response;
 use Encore\Admin\Actions\RowAction;
 use Illuminate\Database\Eloquent\Model;
@@ -16,80 +17,76 @@ class Replicate extends RowAction
 {
     public $name = '会员充值';
 
-    private function calculateEndAt($type, $time, $start): string
+    private function calculateEndAt(int $type, int $times, Carbon $start): Carbon
     {
         // 1天2周3月4季度5半年6全年7终身8次数
-        $value = "";
-        $start = strtotime($start);
+        $value = clone $start;
+
         switch ($type) {
             case 1:
-                $value = $start + $time * 86400;
+                $value = $value->addDays($times);
                 break;
             case 2:
-                $value = $start + ($time * 86400) * 7;
+                $value = $value->addDays(7 * $times);
                 break;
             case 3:
-                $value = $start + ($time * 86400) * 30;
+                $value = $value->addMonths($times);
                 break;
             case 4:
-                $value = $start + ($time * 86400) * 30 * 3;
+                $value = $value->addMonths(3 * $times);
                 break;
             case 5:
-                $value = $start + ($time * 86400) * 30 * 6;
+                $value = $value->addMonths(6 * $times);
                 break;
             case 6:
-                $value = $start + ($time * 86400) * 30 * 12;
+                $value = $value->addYears($times);
                 break;
             case 7:
-                $value = $start + ($time * 86400) * 30 * 100;
-                break;
-            case 8:
-                $value = $start + 1;
+                $value = $value->addYears(100);
                 break;
         }
-
-        return date("Y-m-d H:i:s", (int)$value);
+        return $value;
     }
 
     public function handle(Model $model, Request $request): Response
     {
         // 获取当前用户的会员过期时间
 
-        $type         = $request->post("type");
-        $time         = $request->post("times");
-        $startTime    = $request->post("start_time", date("Y-m-d H:i:s"));
-        $money        = $request->post("money");
-        $weChatId     = $request->post("wechat_id");
-        $remark       = $request->post("remark", "");
-        $gptKeyConfig = UserGptKeys::query()->where("wechat_id", "=", $weChatId)->first(["end_at", "start_at"]);
+        $type = $request->post("type");
+        $times = $request->post("times");
+//        $startTime    = $request->post("start_time", date("Y-m-d H:i:s"));
+        $money = $request->post("money");
+        $weChatId = $request->post("wechat_id");
+        $remark = $request->post("remark", "");
+        $gptKey = UserGptKeys::query()->where("wechat_id", "=", $weChatId)->first(["id", "end_at", "start_at"]);
+        if (empty($gptKey)) {
+            return $this->response()->error('充值失败');
+        }
         // vip原本的开始时间
-        $vipStartTime = $startTime;
-        if (!empty($gptKeyConfig)) {
-            $gptKeyConfig = $gptKeyConfig->toArray();
-            if (strtotime($gptKeyConfig["end_at"]) > time()) { // 会员未过期
-                $startTime    = date("Y-m-d", strtotime($gptKeyConfig["end_at"])) . " " . date("H:i:s");
-                $vipStartTime = $gptKeyConfig["start_at"];
-            }
+        $vipStartTime = Carbon::parse($gptKey->start_at);
+        if (Carbon::parse($gptKey->end_at)->gt(now()) && $times > 0) { // 会员未过期
+            $vipStartTime = Carbon::parse($gptKey->end_at);
         }
         DB::beginTransaction();
         try {
-            if ($type != 8) {
-                $updateRows = UserGptKeys::query()->where("wechat_id", "=", $weChatId)->update([
-                    "start_at" => $vipStartTime,
-                    "end_at" => $this->calculateEndAt($type, $time, $startTime),
-                ]);
-            }
             if ($type == 8) {
-                UserGptKeys::query()->where("wechat_id", "=", $weChatId)->increment("numbers", (int)$time);
+                $gptKey->increment("numbers", (int)$times);
+            } else {
+
+                $gptKey->update([
+                    "start_at" => $vipStartTime,
+                    "end_at" => $this->calculateEndAt($type, $times, $vipStartTime),
+                ]);
+
             }
             $historyModel = UpVipHistory::query()->create([
                 "wechat_id" => $weChatId,
-                "times" => $time,
+                "times" => $times,
                 "money" => $money,
                 "remark" => $remark,
                 "vip_config_id" => $type,
-                "start_time" => $startTime,
-                'end_time' => $this->calculateEndAt($type, $time, $startTime),
+                "start_time" => $vipStartTime,
+                'end_time' => $gptKey->end_at,
             ]);
             if ($historyModel->getKey()) {
                 DB::commit();
@@ -105,8 +102,8 @@ class Replicate extends RowAction
 
     public function form(): void
     {
-        $this->select('type', '充值类型')->options(UpVipConfig::getConfig())->help("选择按次数充值类型时，下列的充值时长表示可使用次数");
-        $this->text('times', '充值时长(次数)')->default(0.00)->rules('required|min:0.01|decimal:0,2')->help("值只能是浮点数，保留1~2位小数");
+        $this->select('type', '充值类型')->options(UpVipConfig::getConfig())->default(1)->rules('required')->help("选择按次数充值类型时，下列的充值时长表示可使用次数");
+        $this->text('times', '数量')->default(1)->rules('required|integer|min:-10000')->help("值只能是整数（可以是负数）最小-10000");
         //$this->datetime("start_time", "开始时间")->help("该时间表示会员正式生效的时间，不勾选则默认会当前时间。");
         $this->text("money", "充值金额")->default(0.01)->rules('required|min:0.01|decimal:0,2')->help("值只能是浮点数，保留1~2位小数");
         $this->hidden("wechat_id", "会员编号")->default($this->getRow()->getAttribute("id"));
--- database/migrations/2023_04_20_150724_create_up_vip_history.php
+++ database/migrations/2023_04_20_150724_create_up_vip_history.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('up_vip_history', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('wechat_id')->comment('用户id');
-            $table->unsignedDecimal('times', 10, 2)->comment('开通时常');
+            $table->decimal('times', 10, 2)->comment('开通时常');
             $table->unsignedDecimal('money', 10, 2)->default('0.00')->comment('金额');
             $table->unsignedInteger('vip_config_id')->comment('VIP配置');
             $table->string('remark')->nullable()->comment('备注');
